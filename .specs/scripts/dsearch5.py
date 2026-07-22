4f432b3a7ddd5e89e7b828780bdd69d663bbc556
Justin Johnson <justinaugustejohnson@gmail.com>
2026-07-22 00:11:24 -0700

    broaden agents.md rule to force all temp files out of root and move lingering script

---
+++ .specs/scripts/dsearch5.py
@@ -0,0 +1,12 @@
+import urllib.request
+import urllib.parse
+import re
+
+url = "https://lite.duckduckgo.com/lite/"
+data = urllib.parse.urlencode({'q': 'site:virtualdj.com/forums "setting" action cycle button'}).encode('utf-8')
+req = urllib.request.Request(url, data=data, headers={'User-Agent': 'Mozilla/5.0'})
+try:
+    html = urllib.request.urlopen(req).read().decode('utf-8')
+    for s in re.findall(r'<td class=\'snippet\'>(.*?)</td>', html, re.IGNORECASE | re.DOTALL):
+        print(re.sub(r'<[^>]+>', '', s).strip())
+except Exception as e: print(e)
